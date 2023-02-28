06fce12253d4377ce453d4b9857d856b9651dc7a
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-02-28 16:37:01 -0700

    [UPDATED]: scores styles

--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -7,5 +7,18 @@ import { Score } from '../../models/score';
   styleUrls: ['./score.component.scss'],
 })
 export class ScoreComponent {
-  @Input() score: Score;
+  @Input() scores: Score[];
+
+  private get topScore() {
+    const counts = this.scores.map((score) => score.count);
+    return Math.max(...counts);
+  }
+
+  private isTopCount(count: number) {
+    return count === this.topScore;
+  }
+
+  public getCountColor (count: number){
+    return this.isTopCount(count) ? 'accent' : 'none'
+  }
 }
